b29124435fba0692b045ae2f7b3217a3a412cb14
Chemslayer <nicolinilr@gmail.com>
2020-11-02 16:14:53 -0800

    Create first framework for Ravenous App

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+//import logo from './logo.svg';
+import BusinessList from '../BusinessList/BusinessList.js';
+import SearchBar from '../SearchBar/SearchBar.js';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ravenous</h1>
+      <SearchBar />
+      <BusinessList />
+    </div>
+  );
+}
+
+export default App;
